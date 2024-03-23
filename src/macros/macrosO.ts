60291056fb006ef30e41eba3f21e74921a664bc0
Chesco216 <franchescofv198@gmail.com>
2024-03-23 18:25:33 -0400

    pipipi

--- src/macros/macrosO.ts
+++ src/macros/macrosO.ts
@@ -1,53 +1,387 @@
 export const macros = [
-    {
-      "nombre": "pollo",
+      {
+        "nombre": "pollo",
+        "categoria": "carne",
+        "macros": {
+          "calorias": 165,
+          "proteinas": 31,
+          "grasa": 3.6,
+          "vitaminas": ["A", "B6", "B12"],
+          "minerales": ["hierro", "potasio", "magnesio"]
+        }
+      },
+      {
+        "nombre": "carne de res",
+        "categoria": "carne",
+        "macros": {
+          "calorias": 250,
+          "proteinas": 26,
+          "grasa": 15,
+          "vitaminas": ["B3", "B6", "B12"],
+          "minerales": ["hierro", "zinc", "selenio"]
+        }
+      },
+      {
+        "nombre": "cerdo",
+        "categoria": "carne",
+        "macros": {
+          "calorias": 242,
+          "proteinas": 25,
+          "grasa": 16,
+          "vitaminas": ["B1", "B3", "B6"],
+          "minerales": ["hierro", "zinc", "selenio"]
+        }
+      },
+      {
+        "nombre": "pavo",
+        "categoria": "carne",
+        "macros": {
+          "calorias": 189,
+          "proteinas": 29,
+          "grasa": 7,
+          "vitaminas": ["B3", "B6", "B12"],
+          "minerales": ["hierro", "zinc", "potasio"]
+        }
+      },
+      {
+        "nombre": "cordero",
+        "categoria": "carne",
+        "macros": {
+          "calorias": 258,
+          "proteinas": 25,
+          "grasa": 16,
+          "vitaminas": ["B2", "B3", "B6"],
+          "minerales": ["hierro", "zinc", "selenio"]
+        }
+      },
+      {
+      "nombre": "zanahoria",
+      "categoria": "vegetales",
       "macros": {
-        "calorias": 165,
-        "proteinas": 31,
-        "grasa": 3.6,
-        "vitaminas": ["A", "B6", "B12"],
-        "minerales": ["hierro", "potasio", "magnesio"]
+        "calorias": 41,
+        "proteinas": 0.9,
+        "grasa": 0.2,
+        "vitaminas": ["A", "K"],
+        "minerales": ["potasio", "calcio", "magnesio"]
       }
     },
     {
-      "nombre": "carne",
+      "nombre": "espinaca",
+      "categoria": "vegetales",
       "macros": {
-        "calorias": 250,
-        "proteinas": 26,
-        "grasa": 15,
-        "vitaminas": ["B3", "B6", "B12"],
-        "minerales": ["hierro", "zinc", "selenio"]
+        "calorias": 23,
+        "proteinas": 2.9,
+        "grasa": 0.4,
+        "vitaminas": ["A", "C", "K"],
+        "minerales": ["hierro", "calcio", "magnesio"]
       }
     },
     {
-      "nombre": "pescado",
+      "nombre": "brócoli",
+      "categoria": "vegetales",
       "macros": {
-        "calorias": 206,
-        "proteinas": 22,
-        "grasa": 13,
-        "vitaminas": ["D", "B3", "B12"],
-        "minerales": ["yodo", "magnesio", "selenio"]
+        "calorias": 34,
+        "proteinas": 2.8,
+        "grasa": 0.4,
+        "vitaminas": ["C", "K"],
+        "minerales": ["potasio", "calcio", "magnesio"]
       }
     },
     {
-      "nombre": "leche",
+      "nombre": "tomate",
+      "categoria": "vegetales",
       "macros": {
-        "calorias": 42,
-        "proteinas": 3.4,
-        "grasa": 1,
-        "vitaminas": ["A", "D", "B12"],
-        "minerales": ["calcio", "potasio", "fósforo"]
+        "calorias": 18,
+        "proteinas": 0.9,
+        "grasa": 0.2,
+        "vitaminas": ["C", "K"],
+        "minerales": ["potasio", "calcio", "magnesio"]
       }
     },
     {
-      "nombre": "huevo",
+      "nombre": "pepino",
+      "categoria": "vegetales",
       "macros": {
-        "calorias": 155,
-        "proteinas": 13,
-        "grasa": 11,
-        "vitaminas": ["A", "D", "B12"],
-        "minerales": ["hierro", "calcio", "zinc"]
+        "calorias": 15,
+        "proteinas": 0.7,
+        "grasa": 0.1,
+        "vitaminas": ["C", "K"],
+        "minerales": ["potasio", "calcio", "magnesio"]
       }
-    }
+    },
+    {
+      "nombre": "cebolla",
+      "categoria": "vegetales",
+      "macros": {
+        "calorias": 40,
+        "proteinas": 1.1,
+        "grasa": 0.1,
+        "vitaminas": ["C", "B6"],
+        "minerales": ["potasio", "calcio", "magnesio"]
+      }
+    },
+    {
+      "nombre": "calabaza",
+      "categoria": "vegetales",
+      "macros": {
+        "calorias": 26,
+        "proteinas": 1,
+        "grasa": 0.1,
+        "vitaminas": ["A", "C", "E"],
+        "minerales": ["potasio", "calcio", "magnesio"]
+      }
+    },
+    {
+      "nombre": "pimiento",
+      "categoria": "vegetales",
+      "macros": {
+        "calorias": 20,
+        "proteinas": 0.9,
+        "grasa": 0.2,
+        "vitaminas": ["C", "B6"],
+        "minerales": ["potasio", "calcio", "magnesio"]
+      }
+    },
+    {
+      "nombre": "coliflor",
+      "categoria": "vegetales",
+      "macros": {
+        "calorias": 25,
+        "proteinas": 1.9,
+        "grasa": 0.3,
+        "vitaminas": ["C", "K"],
+        "minerales": ["potasio", "calcio", "magnesio"]
+      }
+    },
+    {
+      "nombre": "apio",
+      "categoria": "vegetales",
+      "macros": {
+        "calorias": 16,
+        "proteinas": 0.7,
+        "grasa": 0.2,
+        "vitaminas": ["K", "C"],
+        "minerales": ["potasio", "calcio", "magnesio"]
+      }
+    },
+    {
+      "nombre": "manzana",
+      "categoria": "frutas",
+      "macros": {
+        "calorias": 52,
+        "proteinas": 0.3,
+        "grasa": 0.2,
+        "vitaminas": ["C", "A", "K"],
+        "minerales": ["potasio", "calcio", "magnesio"]
+      }
+    },
+    {
+      "nombre": "plátano",
+      "categoria": "frutas",
+      "macros": {
+        "calorias": 89,
+        "proteinas": 1.1,
+        "grasa": 0.3,
+        "vitaminas": ["C", "B6"],
+        "minerales": ["potasio", "magnesio", "fósforo"]
+      }
+    },
+    {
+      "nombre": "naranja",
+      "categoria": "frutas",
+      "macros": {
+        "calorias": 47,
+        "proteinas": 0.9,
+        "grasa": 0.1,
+        "vitaminas": ["C", "A", "B6"],
+        "minerales": ["potasio", "calcio", "magnesio"]
+      }
+    },
+    {
+      "nombre": "uva",
+      "categoria": "frutas",
+      "macros": {
+        "calorias": 67,
+        "proteinas": 0.6,
+        "grasa": 0.2,
+        "vitaminas": ["C", "K"],
+        "minerales": ["potasio", "calcio", "fósforo"]
+      }
+    },
+    {
+      "nombre": "fresa",
+      "categoria": "frutas",
+      "macros": {
+        "calorias": 32,
+        "proteinas": 0.8,
+        "grasa": 0.3,
+        "vitaminas": ["C", "K"],
+        "minerales": ["potasio", "magnesio", "fósforo"]
+      }
+    },
+    {
+      "nombre": "piña",
+      "categoria": "frutas",
+      "macros": {
+        "calorias": 50,
+        "proteinas": 0.5,
+        "grasa": 0.1,
+        "vitaminas": ["C", "B6"],
+        "minerales": ["potasio", "magnesio", "fósforo"]
+      }
+    },
+    {
+      "nombre": "sandía",
+      "categoria": "frutas",
+      "macros": {
+        "calorias": 30,
+        "proteinas": 0.6,
+        "grasa": 0.2,
+        "vitaminas": ["C", "A"],
+        "minerales": ["potasio", "calcio", "magnesio"]
+      }
+    },
+    {
+      "nombre": "kiwi",
+      "categoria": "frutas",
+      "macros": {
+        "calorias": 61,
+        "proteinas": 1.1,
+        "grasa": 0.5,
+        "vitaminas": ["C", "K"],
+        "minerales": ["potasio", "calcio", "magnesio"]
+      }
+    },
+    {
+      "nombre": "mango",
+      "categoria": "frutas",
+      "macros": {
+        "calorias": 60,
+        "proteinas": 0.8,
+        "grasa": 0.4,
+        "vitaminas": ["C", "A"],
+        "minerales": ["potasio", "calcio", "magnesio"]
+      }
+    },
+    {
+      "nombre": "cereza",
+      "categoria": "frutas",
+      "macros": {
+        "calorias": 50,
+        "proteinas": 1.1,
+        "grasa": 0.3,
+        "vitaminas": ["C", "A"],
+        "minerales": ["potasio", "calcio", "magnesio"]
+      }
+    },
+    {
+      "nombre": "lentejas",
+      "categoria": "legumbre",
+      "macros": {
+        "calorias": 116,
+        "proteinas": 9,
+        "grasa": 0.4,
+        "vitaminas": ["B1", "B6", "Folato"],
+        "minerales": ["hierro", "magnesio", "potasio"]
+      }
+    },
+    {
+      "nombre": "garbanzos",
+      "categoria": "legumbre",
+      "macros": {
+        "calorias": 164,
+        "proteinas": 8.9,
+        "grasa": 2.6,
+        "vitaminas": ["B1", "B6", "Folato"],
+        "minerales": ["hierro", "magnesio", "potasio"]
+      }
+    },
+    {
+      "nombre": "frijoles",
+      "categoria": "legumbre",
+      "macros": {
+        "calorias": 333,
+        "proteinas": 21.6,
+        "grasa": 1.5,
+        "vitaminas": ["B1", "B6", "Folato"],
+        "minerales": ["hierro", "magnesio", "potasio"]
+      }
+    },
+    {
+      "nombre": "soja",
+      "categoria": "legumbre",
+      "macros": {
+        "calorias": 173,
+        "proteinas": 16.6,
+        "grasa": 9,
+        "vitaminas": ["B1", "B6", "Folato"],
+        "minerales": ["hierro", "magnesio", "potasio"]
+      }
+    },
+    {
+      "nombre": "alubias",
+      "categoria": "legumbre",
+      "macros": {
+        "calorias": 127,
+        "proteinas": 9,
+        "grasa": 0.5,
+        "vitaminas": ["B1", "B6", "Folato"],
+        "minerales": ["hierro", "magnesio", "potasio"]
+      }
+    },
+    {
+      "nombre": "almendras",
+      "categoria": "fruto seco",
+      "macros": {
+        "calorias": 579,
+        "proteinas": 21.2,
+        "grasa": 49.9,
+        "vitaminas": ["E", "B2", "B3"],
+        "minerales": ["calcio", "magnesio", "fósforo"]
+      }
+    },
+    {
+      "nombre": "nueces",
+      "categoria": "fruto seco",
+      "macros": {
+        "calorias": 654,
+        "proteinas": 15.2,
+        "grasa": 65.2,
+        "vitaminas": ["E", "B1", "B6"],
+        "minerales": ["cobre", "manganeso", "magnesio"]
+      }
+    },
+    {
+      "nombre": "avellanas",
+      "categoria": "fruto seco",
+      "macros": {
+        "calorias": 628,
+        "proteinas": 14.9,
+        "grasa": 60.8,
+        "vitaminas": ["E", "B1", "B6"],
+        "minerales": ["calcio", "magnesio", "fósforo"]
+      }
+    },
+    {
+      "nombre": "cacahuetes",
+      "categoria": "fruto seco",
+      "macros": {
+        "calorias": 567,
+        "proteinas": 25.8,
+        "grasa": 49.2,
+        "vitaminas": ["B3", "B6", "Folato"],
+        "minerales": ["magnesio", "fósforo", "potasio"]
+      }
+    },
+    {
+      "nombre": "piñones",
+      "categoria": "fruto seco",
+      "macros": {
+        "calorias": 673,
+        "proteinas": 13.7,
+        "grasa": 68.4,
+        "vitaminas": ["E", "B1", "B2"],
+        "minerales": ["magnesio", "fósforo", "potasio"]
+      }
+    },
   ]
-
